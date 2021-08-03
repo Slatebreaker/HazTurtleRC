c3c4d5ad500c3af6ffa3fbac61f416640dbcae09
HazmatDrone <drone@hazmatdr.one>
2021-08-03 15:43:49 +1000

    Update turtleserver.lua

--- turtleserver.lua
+++ turtleserver.lua
@@ -1,14 +1,20 @@
-require("lib/hazmat/turtle_remote")
-require("lib/hazmat/keyInterrupt")
+require("/lib/hazmat/turtle_remote")
+require("/lib/hazmat/keyInterrupt")
 local function q_exit() keyInterrupt(keys.q) end
 
 local function ctl()
-    while true do
+    func = nil
+    while func == nil do
         id, msg = rednet.receive(os.computerLabel())
-        if msg = nil then return
-        else rednet.send(id, msg, os.computerLabel())
+        print(id.. "> Received: " .. msg)
+        func, args = turtle_remote(msg)
+        rednet.send(id, msg, os.computerLabel())
     end
 end
 
 rednet.open("left")
-parallel.waitForAny(ctl, q_exit)
+print("Server started. run \"ctl " .. os.computerLabel() .. "\" in your terminal to connect.")
+while true do
+    ctl()
+    parallel.waitForAny(ctl, func)
+end
