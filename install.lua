712635ffe2bac7aed250bc6d5d641fc985c892e3
HazmatDrone <drone@hazmatdr.one>
2021-08-06 08:32:22 +1000

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,7 +2,7 @@ local url="https://raw.githubusercontent.com/HazmatDrone/cc-turtlecode/master/"
 
 function update(file)
 	fs.delete(file);
-	shell.execute("wget \"" + url + file + "\" \"" + file + "\"");
+	shell.execute("wget \"" .. url .. file + "\" \"" .. file .. "\"");
 end
 
 update("install.lua")
