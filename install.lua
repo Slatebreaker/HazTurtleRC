4c5b0ddffcf73eed45cde837b90c3db41f46164e
HazmatDrone <drone@hazmatdr.one>
2021-08-06 09:34:40 +1000

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,8 +1,35 @@
 local url="https://raw.githubusercontent.com/HazmatDrone/cc-turtlecode/master/"
 
-function update(file)
+function uninstall(file)
 	fs.delete(file);
+end
+
+function update(file)
+	uninstall(file)
 	shell.execute("wget", url .. file, file);
 end
 
-update("install.lua")
+local programs = {
+	"/lib/hazmat/digbasic",
+	"/lib/hazmat/keyInterrupt",
+	"/lib/hazmat/keys",
+	"/lib/hazmat/remotestop",
+	"/lib/hazmat/selectblock",
+	"/lib/hazmat/turtle_remote",
+
+	"/lib/yoshi/1x2_bridge",
+	"/lib/yoshi/1x_ceil.lua",
+	"/lib/yoshi/3x2_bridge",
+	"/lib/yoshi/tw",
+	
+	"/ctl.lua"
+	"/dig.lua"
+	"/install.lua"
+	"/msg.lua"
+	"/speaker.lua"
+	"/turtleserver.lua"
+}
+
+for _, program in pairs(programs) do
+	update(program)
+end
