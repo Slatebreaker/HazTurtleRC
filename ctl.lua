e4744f3d979c2305662ecbb41e4988cc893bbe2d
HazmatDrone <drone@hazmatdr.one>
2022-07-06 02:59:25 +1000

    Refactoring to make this slightly more maintainable

--- ctl.lua
+++ ctl.lua
@@ -1,10 +1,11 @@
 require("lib/hazmat/keys")
+local server  = ...
 rednet.open("back")
 local file = fs.open(".lastctl", "w")
-local lastctl = file.write(...)
+local lastctl = file.write(server)
 file.close()
 while true do
     io.write(server .. "> ")
-    print(keyctl(...))
+    print(keyctl(server))
     sleep(0.2)
 end
