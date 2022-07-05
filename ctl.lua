16410a8fb74112cdd3bbe1b36dca87e38eff7dc1
HazmatDrone <drone@hazmatdr.one>
2022-07-06 02:52:30 +1000

    AGAIN

--- ctl.lua
+++ ctl.lua
@@ -1,6 +1,6 @@
 require("lib/hazmat/keys")
 rednet.open("back")
-local file = open(".lastctl", "w")
+local file = fs.open(".lastctl", "w")
 local lastctl = file.write(...)
 file.close()
 while true do
