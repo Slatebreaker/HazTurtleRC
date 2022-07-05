c8805efeaf30323cc146d291dac3cf5815bd856d
HazmatDrone <drone@hazmatdr.one>
2022-07-06 02:42:04 +1000

    Add some small fixes to some scripts

--- startup.lua
+++ startup.lua
@@ -3,4 +3,12 @@ and peripheral.find("modem")
 then
   shell.openTab("turtleserver.lua")
   shell.switchTab(2)
+elseif pocket
+and peripheral.find("modem")
+then
+  local file = open(.lastctl, "r")
+  local lastctl = file.read()
+  file.close()
+  shell.openTab("ctl "..lastctl)
+  shell.switchTab(2)
 end
